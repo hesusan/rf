c4ad8c35c52266935bb3357af6bd1b2afe958d9b
hesusan <nasuseh@gmail.com>
2015-07-31 08:17:00 +0900

    pc:パーツ追加

---
+++ app/start/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>国内最大のクラウドファウンディングサイト READYFOR</title>
+<meta name="viewport" content="width=device-width">
+<meta name="keywords" content="">
+<meta name="description" content="国内最大のクラウドファウンディングサイトREADYFORは、誰もがやりたいことを実行できる世界を創ります">
+<meta property="og:title" content="国内最大のクラウドファウンディングサイト READYFOR">
+<meta property="og:description" content="国内最大のクラウドファウンディングサイトREADYFORは、誰もがやりたいことを実行できる世界を創ります">
+<meta property="og:type" content="website">
+<meta property="og:url" content="">
+<meta property="og:image" content="">
+<meta property="og:site_name" content="国内最大のクラウドファウンディングサイト READYFOR">
+<link rel="stylesheet" href="/css/style.css">
+<!--[if lt IE 9]>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+<div class="Site-container" data-page-container>
+
+  <? require_once '../php/header.php'; ?>
+
+  <div class="Page-body" style="padding-top: 20px">
+
+
+    <header class="Header-1">
+      <h1 class="Heading-1">プロジェクト達成までの流れ</h1>
+    </header>
+
+    <header class="Header-1">
+      <h1 class="Heading-1">READYFORで活動を成功した人たちの声</h1>
+    </header>
+
+    <header class="Header-1">
+      <h1 class="Heading-1">夢をかなえる準備はできましたか？</h1>
+    </header>
+
+    <p class="u-align_c u-txt_14">
+      小さなことでも、お気軽にご相談ください。<br>READYFORのスタッフから３営業日位内にご連絡させていただきます。<br>
+      ご相談の過程でプロジェクトの内容が固まってきた場合は、いつでもアイディアの申請に進んでいただくことができます。
+    </p>
+
+    <div class="Container">
+      <div>
+        <a href="#" class="Btn-1">
+          <div class="Btn-1__inner">
+            <div class="Btn-1__content">
+              <div class="Btn-1__txt">相談する</div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a href="#" class="Btn-1">
+          <div class="Btn-1__inner">
+            <div class="Btn-1__content">
+              <div class="Btn-1__txt u-txt_18">すぐにプロジェクトをはじめたい人はこちら</div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a href="#" class="Btn-1">
+          <div class="Btn-1__inner">
+            <div class="Btn-1__content">
+              <div class="Btn-1__txt u-txt_18">相談してみる</div>
+              <div class="u-txt_12 u-mt_5">スタッフに内容や金額の相談に乗って欲しい！</div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+      <div>
+        <a href="#" class="Btn-1">
+          <div class="Btn-1__inner">
+            <div class="Btn-1__content">
+              <div class="Btn-1__txt u-txt_18">プロジェクトを始める</div>
+              <div class="u-txt_12 u-mt_5">担当者と一緒にプロジェクトの掲載準備をしたい！</div>
+            </div>
+          </div>
+        </a>
+      </div>
+
+    </div>
+
+  </div>
+
+  <? require_once '../php/footer.php'; ?>
+
+</div>
+
+<!--
+<script src="js/index.js"></script>
+-->
+
+</body>
+</html>
