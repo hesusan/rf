c35609ca6a3b595617eed65f8e4983c3fdc07c95
hesusan <nasuseh@gmail.com>
2015-08-09 23:15:20 +0900

    login/signupコード配置

--- app/index.php
+++ app/index.php
@@ -28,9 +28,22 @@
 
   <aside class="Modal Signup u-d_none">
     <a href="#" class="Modal__closer">×閉じる</a>
-    <h1 class="u-fs_24 u-align_c u-mb_25">新規登録</h1>
-    <div class="u-mb_15">facebookで登録</div>
-    <p class="u-mb_15 u-align_c">または</p>
+    <h1 class="u-fs_20 u-align_c u-mb_25">新規登録</h1>
+
+    <div class="u-mb_15">
+      <a href="#" class="Btn-2 is-blue">
+        <span class="Btn-2__inner">
+          <span class="Btn-2__content">
+            <span class="Btn-2__icon Icon-fb"></span><span class="Btn-2__txt">Facebookで登録</span>
+          </span>
+        </span>
+      </a>
+    </div>
+
+    <div class="Modal__part-0 u-mb_15 u-align_c">
+      <div class="Modal__part-0__circle">または</div>
+    </div>
+
     <form>
       <div>
         <input type="text" placeholder="メールアドレス" class="Input u-fit-w">
@@ -44,13 +57,69 @@
       <div class="u-mt_15">
         <input type="text" placeholder="お名前(ニックネーム)" class="Input u-fit-w">
       </div>
-      <div class="u-mt_25">メールアドレスで登録</div>
+
+      <div class="u-mt_25">
+        <button class="Btn-2 is-red">
+          <span class="Btn-2__inner">
+            <span class="Btn-2__content">
+              <span class="Btn-2__icon Icon-mail"></span><span class="Btn-2__txt">メールアドレスで登録</span>
+            </span>
+          </span>
+        </button>
+      </div>
+
     </form>
     <p class="u-mt_25 u-fs_16 u-align_c">
       アカウントを既にお持ちの方は<a href="#">ログイン</a>
     </p>
     <p class="u-mt_25 u-fs_12 u-note">
       アカウントを作成することにより、<a href="#">利用規約</a>および<a href="#">プライバシーポリシー</a>に同意されたものとさせていただきます。
+    </p>
+  </aside>
+
+  <aside class="Modal Login u-d_none">
+    <a href="#" class="Modal__closer">×閉じる</a>
+    <h1 class="u-fs_20 u-align_c u-mb_25">ログイン</h1>
+
+    <div class="u-mb_15">
+      <a href="#" class="Btn-2 is-blue">
+        <span class="Btn-2__inner">
+          <span class="Btn-2__content">
+            <span class="Btn-2__icon Icon-fb"></span><span class="Btn-2__txt">Facebookでログイン</span>
+          </span>
+        </span>
+      </a>
+    </div>
+
+    <div class="Modal__part-0 u-mb_15 u-align_c">
+      <div class="Modal__part-0__circle">または</div>
+    </div>
+
+    <form>
+      <div>
+        <input type="text" placeholder="メールアドレスを入力" class="Input u-fit-w">
+      </div>
+      <div class="u-mt_15">
+        <input type="text" placeholder="パスワードを入力" class="Input u-fit-w">
+      </div>
+
+      <p class="u-align_c u-mt_15 u-mb_50">
+        <a href="#">パスワードをお忘れの方</a>
+      </p>
+
+      <div class="u-mt_25">
+        <button class="Btn-2 is-red">
+          <span class="Btn-2__inner">
+            <span class="Btn-2__content">
+              <span class="Btn-2__icon Icon-mail"></span><span class="Btn-2__txt">メールアドレスでログイン</span>
+            </span>
+          </span>
+        </button>
+      </div>
+    </form>
+
+    <p class="u-mt_60 u-fs_16 u-align_c">
+      アカウントをまだお持ちでない方<a href="#">新規登録</a>
     </p>
   </aside>
 
