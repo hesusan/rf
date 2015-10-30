254626494c422c7560f08f7f99a7c8d0281f4993
nishio.ken <nishio.ken@dena.jp>
2015-10-30 11:13:52 +0900

    デザインに無いため削除

--- app/sp/php/scroller.php
+++
@@ -1,7 +0,0 @@
-<!--
-<div class="Site-scroller Container">
-  <a href="#" class="Site-scroller__main is-type0">
-    <div class="Site-scroller__pagetop is-type0">このページのTOPへ</div>
-  </a>
-</div>
--->
